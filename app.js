a5a80ed966325e7b26c5be9801e0e09648adbdaf
lakshay chauhan <lakshaychauhan030@gmail.com>
2022-04-24 15:55:39 +0530

    Complete Level 2 - Database Encryption

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -14,10 +15,12 @@ app.use(
 
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-}
+});
+const secret = "Iamasoftwareengineer";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -41,7 +44,7 @@ app.post("/register", (req, res) => {
             console.log(e);
         else
             res.render("secrets");
-    });
+    }); 
 });
 
 app.post("/login", (req, res) => {
